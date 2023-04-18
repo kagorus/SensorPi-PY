8d6ffc0145a35726c5fd65c2fe34fd44d0c18015
Kagorus <nathan@kagorus.net>
2023-04-18 11:45:39 +0100

    Update sensors.py

--- sensors.py
+++ sensors.py
@@ -9,3 +9,19 @@ tempReadings = []
 humidityReadings = []
 tempAverage = float(0)
 humidAverage = float(0)
+
+def append_readings():
+    global tempAverage
+    global humidAverage
+    if len(tempReadings) != 60:
+        tempReadings.append(temperature_c)
+    else:
+        del tempReadings[0]
+        tempReadings.append(temperature_c)
+    if len(humidityReadings) != 60:
+        humidityReadings.append(humidity)
+    else:
+        del tempReadings[0]
+        humidityReadings.append(humidity)
+    tempAverage = sum(tempReadings) / len(tempReadings)
+    humidAverage = sum(humidityReadings) / len(humidityReadings)
